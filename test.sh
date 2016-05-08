4793685f65ea5738b17f74337daeae7f63135a9c
july0919 <poodol0919@gmail.com>
2016-05-08 16:28:04 +0900

    PiCamera test

---
+++ test.sh
@@ -0,0 +1,3 @@
+export STREAMER_PATH=$HOME/mjpg-streamer/mjpg-streamer-experimental
+export LD_LIBRARY_PATH=$STREAMER_PATH
+$STREAMER_PATH/mjpg_streamer -i "input_raspicam.so" -o "output_http.so -w $STREAMER_PATH/www"
