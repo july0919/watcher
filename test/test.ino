cc74bfdf98477568d13dfa19c0a92ca7144cc1ba
july0919 <poodol0919@gmail.com>
2016-06-08 17:37:51 +0900

    Arduino control program

---
+++ test/test.ino
@@ -0,0 +1,59 @@
+//  Sharp GP2xx IR sensor
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+SoftwareSerial MySerial(4, 5); // RX, TX
+Servo MyServo;
+
+int count = 0;
+
+void setup(){
+  MyServo.attach(9);
+  Serial.begin(9600);
+  MySerial.begin(9600);
+}
+
+void loop(){
+
+  int sensorValue = analogRead(A0);                    //sensor input
+  float voltage = sensorValue*5.0/1024;             //voltage mapping
+  float distance =27.15*pow(voltage,-1.179);    //by curve fitting
+
+  if(distance < 60) {
+    count++;
+    if(count==1)
+    Serial.print('d');
+  }
+  
+  while(MySerial.available()) {
+    int data = MySerial.read();
+    if(data == 111) {
+      for (int pos = 45; pos <= 150; pos += 1) {
+        MyServo.write(pos);
+        delay(5);
+      }
+    }
+    if(data == 98) {
+      for(int pos = 150; pos>=45; pos-=1) {
+        MyServo.write(pos);
+        delay(5);
+      }
+    }
+  }
+
+  while(Serial.available()) {
+    int data = Serial.read();
+    if(data == 111) {
+      for (int pos = 45; pos <= 150; pos += 1) {
+        MyServo.write(pos);
+        delay(5);
+      }
+
+      delay(10000);
+      for(int pos = 150; pos>=45; pos-=1) {
+        MyServo.write(pos);
+        delay(5);
+      }
+    }
+  }  
+}
